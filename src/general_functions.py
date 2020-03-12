e5050573d0daab790793da582d792f4892a812d0
dstamos <dimitrios.i.stamos@gmail.com>
2020-03-12 14:29:44 +0000

    Multiclass subgradient and loss.

--- src/general_functions.py
+++ src/general_functions.py
@@ -10,11 +10,23 @@ def l2_unit_ball_projection(vector):
 
 def subgradient(x, y, w, loss_name=None):
     if loss_name == 'hinge':
-        score = x @ w
-        if y * score < 1:
-            return -y
-        else:
-            return 0
+        pred_scores = x @ w
+        n_classes = w.shape[1]
+
+        indicator_part = np.ones(pred_scores.shape)
+        indicator_part[y] = 0
+
+        true = pred_scores[y]
+        true = np.tile(true, (1, n_classes))
+
+        j_star = np.argmax(indicator_part + pred_scores - true)
+
+        subgrad = np.zeros(w.shape)
+
+        if y != j_star:
+            subgrad[:, j_star] = x
+            subgrad[:, y] = -x
+        return subgrad
     elif loss_name == 'absolute':
         pred = x @ w
         if y < pred:
@@ -27,16 +39,29 @@ def subgradient(x, y, w, loss_name=None):
 
 def loss(x, y, w, loss_name='hinge'):
     if loss_name == 'hinge':
-        # from sklearn.metrics import hinge_loss
-        # if hasattr(y, '__len__'):
-        #     return 1 / len(y) * hinge_loss(y, x @ w)
-        # else:
-        #     return hinge_loss([y], x.reshape(1, -1) @ w)
+        def multiclass_hinge_loss(curr_labels, pred_scores):
+            indicator_part = np.ones(pred_scores.shape)
+            indicator_part[np.arange(pred_scores.shape[0]), curr_labels] = 0
 
+            true = pred_scores[np.arange(pred_scores.shape[0]), curr_labels].reshape(-1, 1)
+            true = np.tile(true, (1, 4))
+
+            loss = np.max(indicator_part + pred_scores - true, axis=1)
+            loss = np.sum(loss) / len(curr_labels)
+            return loss
         if hasattr(y, '__len__'):
-            return (1 / len(y)) * np.sum(np.maximum(0, 1 - y * (x @ w)))
+            return multiclass_hinge_loss(y, x @ w)
         else:
-            return np.maximum(0, 1 - y * (x @ w))
+            scores = x @ w
+            n_classes = w.shape[1]
+            indicator_part = np.ones(scores.shape)
+            indicator_part[y] = 0
+
+            true = scores[y]
+            true = np.tile(true, (1, n_classes))
+
+            return np.max(indicator_part + scores - true, axis=1)[0]
+
     elif loss_name == 'absolute':
         if hasattr(y, '__len__'):
             return 1 / len(y) * np.sum(np.abs(y - x @ w))
